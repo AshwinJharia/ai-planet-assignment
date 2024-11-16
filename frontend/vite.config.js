818b8fef5afaf88902a35f1fff66ce602e1a4ec0
AshwinJharia <ashwinjharia48@gmail.com>
2024-11-17 00:23:09 +0530

    add dotenv

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { loadEnv } from 'vite';
+import dotenv from 'dotenv';
 
-const env = loadEnv(process.env.NODE_ENV, process.cwd(), 'VITE_');
+dotenv.config()
 
-console.log('VITE_BACKEND:', env.VITE_BACKEND)  // Log for debugging
+// const env = loadEnv(process.env.NODE_ENV, process.cwd(), 'VITE_');
+
+console.log('VITE_BACKEND:', process.env.VITE_BACKEND)  // Log for debugging
 
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: env.VITE_BACKEND || 'http://localhost:5000', // Fallback
+        target: process.env.VITE_BACKEND || 'http://localhost:5000', // Fallback
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
